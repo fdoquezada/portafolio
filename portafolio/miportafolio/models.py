19f39694f9aa4c46bd4eb3ac6420689552dd2261
Ubuntu <ubuntu@ip-172-31-86-55.ec2.internal>
2022-09-21 03:31:02 +0000

    actualizacion de portafolio

--- portafolio/miportafolio/models.py
+++ portafolio/miportafolio/models.py
@@ -9,7 +9,7 @@ from django.db.models.fields.files import ImageField
 # Create your models here.
 class project(models.Model):
     title = CharField(max_length=100)
-    descripcion = CharField(max_length = 150)
+    description = CharField(max_length = 150)
     image =ImageField(upload_to='miportafolio/images/')
     url = URLField(blank=True)
     
--- portafolio/miportafolio/views.py
+++ portafolio/miportafolio/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import project 
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    projects = project.objects.all()
+
+    return render(request, 'home.html', {'projects': projects})
 
